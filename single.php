d338d99e964e2be649e93c45014d969b8c80abd0
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-20 16:32:21 +0000

    continuing index & starting single.php

--- single.php
+++ single.php
@@ -5,6 +5,14 @@
         echo "<section class='index'>";
         while (have_posts()):
             the_post();
+            $tags = get_the_tags();
+            if ($tags) {
+                $tag_links = array(); // Create an array to hold the tag links
+
+                foreach ($tags as $tag) {
+                    $tag_links[] = '<a href="' . esc_url(get_tag_link($tag->term_id)) . '">' . esc_html($tag->name) . '</a>';
+                }
+            }
     ?>
             <article>
                 <div class="metainfo">
@@ -12,9 +20,11 @@
                         <h3><?php the_time('j F Y') ?></h3>
                         <h3><?php the_author() ?> </h3>
                     </div>
+                    <div class="line2 tags">
+                        <?php echo implode(' · ', $tag_links); ?>
+                    </div>
                 </div>
                 <h2><?php the_title(); ?></h2>
-                <?php the_post_thumbnail(); ?> 
                 <?php the_excerpt(); ?>
             </article>
     <?php
