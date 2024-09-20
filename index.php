df6707a558c2e711f604d9cc6954a74d77b79bdf
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-20 15:56:42 +0000

    finished footer & started index.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
 <main>
-    test
+<?php
+        if (have_posts()):
+            while(have_posts()):
+                the_post();
+                //the_title('<h3>','</h3>');
+            ?> 
+            <article>
+            <h2><?php the_title();?></h2>
+            <h6>Contenu :<h6><?php the_content();
+            endwhile; ?>
+            </article>
+    <?php
+        else:
+            echo "You don't have any posts for index.php";
+            echo "<script>console.log('You don't have any posts for index.php')</script>";
+endif; ?>
 </main>
 <?php get_footer(); ?>
