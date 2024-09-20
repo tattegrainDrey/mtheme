ff6dc997a026b5f76d9ced380d27495afb66ae83
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-20 16:36:16 +0000

    adding support for thumbnails

--- functions.php
+++ functions.php
@@ -1,17 +1,19 @@
 <?php
 
 /* -------------------------------------- Style */
-function enqueue_styles() {
+function enqueue_styles()
+{
     wp_enqueue_style('my-theme-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
 
 /* -------------------------------------- Theme support */
-add_theme_support( 'custom-logo', array(
+add_theme_support('custom-logo', array(
     'height' => 55,
     'width'  => 'auto'
-    ) );
+));
 
+add_theme_support('post-thumbnails');
 /* -------------------------------------- Widgets*/
 function enregistrer_sidebar()
 {
