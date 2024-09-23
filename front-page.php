103f3361c6827b474f9410b5b0921827f631421b
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-23 01:46:31 +0000

    test 3 for homepage

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main id="main">
     <?php
-    if ((is_home() || is_front_page()) && have_posts()):
+    if (have_posts()):
         echo "<section id='section' class='home'>";
         while (have_posts()):
             the_post();
