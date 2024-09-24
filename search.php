b7fbb280680840a0d4e31d53033bcb0aed1d533c
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-24 07:05:28 +0000

    debugging search.php 4 + helping header

--- search.php
+++ search.php
@@ -73,7 +73,10 @@
         }
         echo "</section>";
     else :
-        echo "You don't have any posts for index.php";
+        echo "<section>";
+            echo "<div> We haven't found anything pertaining to". __('', 'text-domain') .". Please try another query. </div>";
+            echo get_search_form();
+        echo "<section>";
         echo "<script>console.log('You don't have any posts for index.php')</script>";
     endif; ?>
 </main>
