bf204c45e845a06e6cfc7a70ed26a40525331a38
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-24 06:43:10 +0000

    debugging search.php

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form id="search" role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
-    <input type="search" name="search">
-    <button type="submit">
+<form id="searchform" role="search" method="get" action="<?php echo home_url('/'); ?>">
+    <input type="search" name="search" value="<?php echo get_search_query() ?> "> 
+    <button type="submit" value="<?php esc_attr_e('Search'); ?>">
         <img src="<?php echo esc_url(get_template_directory_uri() . '/icons/search.svg'); ?>" alt="search ico">
     </button>
 </form>
