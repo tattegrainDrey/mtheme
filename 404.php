ceaf272a88e10e420ba30eb6f01466e7f2b18427
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-24 08:17:09 +0000

    adding 404 test 4

--- 404.php
+++ 404.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 <main id="main">
-            <article>
-                I think you're lost... You can either use the button above to go back or use the search bar.
-                <?php get_search_form() ?> 
-            </article>
+    <section id="section">
+        <article>
+            I think you're lost... You can either use the button above to go back or use the search bar.
+            <?php get_search_form() ?>
+        </article>
+    </section>
 </main>
 <?php get_footer(); ?>
