5b45fd2d21b300c290fbc7eef1095a6977c340ef
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-24 08:18:48 +0000

    styling 404

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <main id="main">
-    <section id="section">
+    <section id="section" class="error">
         <article>
             I think you're lost... You can either use the button above to go back or use the search bar.
             <?php get_search_form() ?>
