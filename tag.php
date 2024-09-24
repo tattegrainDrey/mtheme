e677b7c4e89356b022a5c6cdd859501b1862055f
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-24 06:14:58 +0000

    making tag.php 8

--- tag.php
+++ tag.php
@@ -5,7 +5,7 @@
         echo "<section id='section' class='index'>";
         echo '<button class="back" onclick="window.history.back();">Go Back</button>';
         ?> 
-        <h1><?php single_tag_title(); ?> </h1>
+        <h1><?php single_tag_title('Tag: ', true); ?></h1>
         <?php 
         while (have_posts()):
             the_post();
