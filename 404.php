ce6bfb3af6e3877f2d3f4c3ff99c7959ccb2f162
D.A.A. Tattegrain <91336108+tattegrainDrey@users.noreply.github.com>
2024-09-24 08:22:14 +0000

    styling 404 again

--- 404.php
+++ 404.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
 <main id="main">
     <section id="section" class="error">
-        <article>
-            I think you're lost... You can either use the button above to go back or use the search bar.
+            <h3>I think you're lost... You can either use the button above to go back or use the search bar.</h3>
             <?php get_search_form() ?>
-        </article>
     </section>
 </main>
 <?php get_footer(); ?>
